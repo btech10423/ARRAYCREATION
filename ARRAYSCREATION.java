4a2f85203fc1591f636194a4abb7475d64d48921
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-11-26 13:56:45 +0530

    Add files via upload

---
+++ ARRAYSCREATION.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ArraysCC{
+    public static void main(strings args[])
+    {
+        int marks[] = new int[50];
+        int numbers[] = {1, 2, 3};
+    }
+   
+}
