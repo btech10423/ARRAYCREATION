2836d2c91d19dfa3b6b79d12fcad972942f1b3f3
VAIBHAV~G <88087669+btech10423@users.noreply.github.com>
2022-11-29 21:34:50 +0530

    Add files via upload

---
+++ linearsearch.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class linearsearch{
+    public static int Linearsearch(int numbers[], int key)
+    {
+for(int i=0; i<numbers.length; i++)
+{
+    if(numbers[i]==key)
+    {
+        return i;
+    }
+    else
+    {
+        return -1;
+    }
+}
+return -1;
+    }
+     public static void main(String args[]) 
+    {
+        int numbers[] = {10,20,30,40,50};
+        int key = 50;
+
+        int index = Linearsearch(numbers, key);
+        if(index==-1)
+        {
+        System.out.println("Not found");
+        }
+        else
+        {
+        System.out.println("key is at index:"+index);
+        }
+    }
+}
